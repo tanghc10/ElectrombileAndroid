b6ab2c16ef41c4503f998d24f0c63c4fb7d9ac5a
Jason <Jerson.G@gmail.com>
2016-10-28 15:10:20 +0800

    add service package

---
+++ app/src/main/java/com/xiaoantech/electrombile/services/BaseService.java
@@ -0,0 +1,13 @@
+package com.xiaoantech.electrombile.services;
+
+import android.app.Service;
+
+/**
+ * Created by jk on 16-10-28.
+ *
+ * base class for all service
+ *
+ */
+
+public abstract class BaseService extends Service {
+}
