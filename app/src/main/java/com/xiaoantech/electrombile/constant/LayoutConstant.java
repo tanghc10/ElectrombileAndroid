3b9159bf8fa427083f32044809bdc7431a4d9dac
1207241271 <1207241271@qq.com>
2016-11-01 20:27:35 +0800

    FragmentAdapter

---
+++ app/src/main/java/com/xiaoantech/electrombile/constant/LayoutConstant.java
@@ -0,0 +1,12 @@
+package com.xiaoantech.electrombile.constant;
+
+/**
+ * Created by yangxu on 2016/11/1.
+ */
+
+public class LayoutConstant {
+    public static final int MainPage        =   1;
+    public static final int InfoPage        =   2;
+    public static final int SettingPage     =   3;
+
+}
