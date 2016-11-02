f668b3ab20a7cda51504ccfae41fcfe34a93bc9a
1207241271 <1207241271@qq.com>
2016-11-02 19:58:38 +0800

    Mqtt协议框架
    1.实现部分MQTT协议，有待完善

---
+++ app/src/main/java/com/xiaoantech/electrombile/constant/ServiceConstant.java
@@ -0,0 +1,12 @@
+package com.xiaoantech.electrombile.constant;
+
+/**
+ * Created by yangxu on 2016/11/2.
+ */
+
+public class ServiceConstant {
+    public static String MQTT_HOST = "120.25.157.233"; //tcp://120.25.157.233:1883
+    public static final int PORT = 1883;
+    public static int MQTT_QUALITY_OF_SERVICE = 0;
+
+}
