7e25b6c7d98aed5790a8441807ed18f8b6154335
1207241271 <1207241271@qq.com>
2016-11-03 15:16:09 +0800

    FragmentAdapter

--- app/src/main/java/com/xiaoantech/electrombile/ui/main/FragmentMainActivity.java
+++ app/src/main/java/com/xiaoantech/electrombile/ui/main/FragmentMainActivity.java
@@ -10,6 +10,9 @@ import android.view.View;
 import android.widget.RadioButton;
 
 import com.xiaoantech.electrombile.R;
+import com.xiaoantech.electrombile.ui.main.InfoFragment.Fragment.InfoFragment;
+import com.xiaoantech.electrombile.ui.main.MainFragment.Fragment.MainFragment;
+import com.xiaoantech.electrombile.ui.main.SettingFragment.Fragment.SettingFragment;
 
 import java.util.ArrayList;
 
@@ -36,6 +39,10 @@ public class FragmentMainActivity extends FragmentActivity {
         mainBtn.setOnClickListener(new radioBtnListener(0));
         infoBtn.setOnClickListener(new radioBtnListener(1));
         settingBtn.setOnClickListener(new radioBtnListener(2));
+
+        fragmentArrayList.add(new MainFragment());
+        fragmentArrayList.add(new InfoFragment());
+        fragmentArrayList.add(new SettingFragment());
     }
 
     public class radioBtnListener implements View.OnClickListener{
@@ -52,8 +59,24 @@ public class FragmentMainActivity extends FragmentActivity {
     private void initViewPager(){
         mViewPager = (ViewPager)findViewById(R.id.viewPager_Fragment);
         mViewPager.setAdapter(new FragmentAdapter(getSupportFragmentManager(),fragmentArrayList));
+        mViewPager.addOnPageChangeListener(new MyOnPageChangeListener());
         mViewPager.setCurrentItem(0);
     }
 
+    public class MyOnPageChangeListener implements ViewPager.OnPageChangeListener{
+        @Override
+        public void onPageSelected(int position) {
 
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+    }
 }
---
+++ app/src/main/java/com/xiaoantech/electrombile/ui/main/MainFragment/Fragment/MainFragment.java
@@ -0,0 +1,10 @@
+package com.xiaoantech.electrombile.ui.main.MainFragment.Fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by yangxu on 2016/11/3.
+ */
+
+public class MainFragment extends Fragment {
+}
